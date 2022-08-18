76eff85a73e269f9ed66161080efbc456fe6139e
Artem Zhikharev <artemziharew@gmail.com>
2022-08-18 20:51:57 +0300

    Реализовано подключение к удалённому серверу

--- core-model/src/main/java/com/trialbot/core_model/constants/Constants.kt
+++ core-model/src/main/java/com/trialbot/core_model/constants/Constants.kt
@@ -2,7 +2,7 @@ package com.trialbot.core_model.constants
 
 object Constants {
 
-    const val BASE_URL = "http://10.0.2.2:8080/api/"
+    const val BASE_URL = "https://trialbot-task-oasis-server.herokuapp.com/api/"
 
     const val APP_LOGGING_TAG = "TAG"
 }
