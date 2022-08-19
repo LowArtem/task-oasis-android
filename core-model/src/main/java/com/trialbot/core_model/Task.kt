c4bb3533c1b7f45055d2710425aec412f3a2ef13
Artem Zhikharev <artemziharew@gmail.com>
2022-08-19 19:20:54 +0300

    Подредактированы модели Task

--- core-model/src/main/java/com/trialbot/core_model/Task.kt
+++ core-model/src/main/java/com/trialbot/core_model/Task.kt
@@ -14,6 +14,7 @@ data class Task(
     val description: String?,
     val repeatingInterval: RepeatingInterval,
     val notification: Instant?,
+    val completionDate: Instant?,
     val id: Int?,
     val subtasks: Set<Subtask>
 )
--- feature-tasks/src/main/java/com/trialbot/feature_tasks/data/model/TaskShortResponse.kt
+++ feature-tasks/src/main/java/com/trialbot/feature_tasks/data/model/TaskShortResponse.kt
@@ -3,25 +3,40 @@ package com.trialbot.feature_tasks.data.model
 import com.trialbot.core_model.enum.Difficulty
 import com.trialbot.core_model.enum.Priority
 import java.time.Instant
+import java.util.Date
 
 data class TaskShortResponse(
     val name: String,
-    val deadline: Instant,
+    val deadline: Date?,
     val status: Boolean,
     val difficulty: Int,
     val priority: Int,
     val hasNotification: Boolean,
     val hasRepeat: Boolean,
+    val completionDate: Date?,
     val id: Int
 )
 
 data class TaskShortDto(
     val name: String,
-    val deadline: Instant,
+    val deadline: Instant?,
     val status: Boolean,
     val difficulty: Difficulty,
     val priority: Priority,
     val hasNotification: Boolean,
     val hasRepeat: Boolean,
+    val completionDate: Instant?,
     val id: Int
+)
+
+fun TaskShortResponse.toDto() = TaskShortDto(
+    name = name,
+    deadline = deadline?.toInstant(),
+    status = status,
+    difficulty = Difficulty.values()[difficulty],
+    priority = Priority.values()[priority],
+    hasNotification = hasNotification,
+    hasRepeat = hasRepeat,
+    completionDate = completionDate?.toInstant(),
+    id = id
 )
