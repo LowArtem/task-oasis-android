274a436e49dc88b51c70f869267f08e99c98ad39
Artem Zhikharev <artemziharew@gmail.com>
2022-08-22 17:44:27 +0300

    Добавлен новый вариант поля ввода

--- core-uicomponents/src/main/java/com/trialbot/core_uicomponents/components/InputLineField.kt
+++ core-uicomponents/src/main/java/com/trialbot/core_uicomponents/components/InputLineField.kt
@@ -1,7 +1,7 @@
 package com.trialbot.core_uicomponents.components
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Icon
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -20,27 +21,18 @@ import com.trialbot.core_designsystem.ui.TaskOasisIcons
 import com.trialbot.core_designsystem.ui.theme.TaskOasisTheme
 import com.trialbot.core_designsystem.ui.theme.inputStrokeColor
 
-/**
- * Input field with hint when it blank.
- * Can be the password field if [KeyboardType] of [keyboardOptions] is equal to [KeyboardType.Password].
- *
- * Important thing: you need to deal with [Modifier.sizeIn] instead of plain size.
- * You need to specify height as a minHeight and let it become a bit bigger to error text can be shown.
- *
- * @param isLabelAboveVisible don't use it as state, it is just start and immutable value
- */
 @Composable
-fun InputHintField(
+fun InputLineField(
     modifier: Modifier = Modifier,
     value: String,
-    hint: String,
+    placeholder: String,
     onValueChanged: (String) -> Unit,
     keyboardOptions: KeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
     keyboardActions: KeyboardActions = KeyboardActions.Default,
     error: String = "",
     isError: Boolean = error.isNotEmpty(),
     passwordVisibility: Boolean? = null,
-    isLabelAboveVisible: Boolean = true,
+    backgroundColor: Color = MaterialTheme.colors.background,
     trailingIcon: @Composable (() -> Unit)? = {
         if (isError)
             Icon(
@@ -48,29 +40,24 @@ fun InputHintField(
                 contentDescription = "error",
                 tint = MaterialTheme.colors.error
             )
-    },
-) {
-    var label: (@Composable () -> Unit)? = null
-    if (isLabelAboveVisible) {
-        label = {
-            Text(
-                text = hint
-            )
-        }
     }
-
-    OutlinedTextFieldValidation(
+) {
+    FilledTextFieldValidation(
         modifier = modifier,
         value = value,
+        placeholder = {
+            Text(
+                text = placeholder
+            )
+        },
         onValueChange = onValueChanged,
-        label = label,
         colors = TextFieldDefaults.textFieldColors(
             textColor = MaterialTheme.colors.onSurface,
             unfocusedLabelColor = MaterialTheme.colors.inputStrokeColor,
             focusedLabelColor = MaterialTheme.colors.secondary,
             unfocusedIndicatorColor = MaterialTheme.colors.inputStrokeColor,
             focusedIndicatorColor = MaterialTheme.colors.secondary,
-            backgroundColor = MaterialTheme.colors.surface
+            backgroundColor = backgroundColor
         ),
         error = error,
         isError = isError,
@@ -85,19 +72,23 @@ fun InputHintField(
 
 @Preview(showBackground = true)
 @Composable
-fun InputHintFieldPreview() {
+fun InputLineFieldPreview() {
     TaskOasisTheme() {
         var text by remember { mutableStateOf("") }
 
-        InputHintField(
-            modifier = Modifier
-                .padding(top = 134.dp)
-                .sizeIn(maxWidth = 336.dp, minHeight = 63.dp),
-            value = text,
-            hint = "Input text",
-            onValueChanged = { text = it },
-            isError = text == "error",
-            error = if (text == "error") "Error text" else ""
-        )
+        Box(
+            Modifier.fillMaxSize().background(MaterialTheme.colors.background)
+        ) {
+            InputLineField(
+                modifier = Modifier
+                    .padding(top = 134.dp)
+                    .sizeIn(maxWidth = 336.dp, minHeight = 63.dp),
+                value = text,
+                placeholder = "Input text",
+                onValueChanged = { text = it },
+                isError = text == "error",
+                error = if (text == "error") "Error text" else ""
+            )
+        }
     }
 }
---
+++ core-uicomponents/src/main/java/com/trialbot/core_uicomponents/components/TextFieldValidation.kt
@@ -0,0 +1,186 @@
+package com.trialbot.core_uicomponents.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.trialbot.core_designsystem.ui.TaskOasisIcons
+import com.trialbot.core_designsystem.ui.theme.TaskOasisTheme
+import com.trialbot.core_designsystem.ui.theme.disabledColor
+
+@Composable
+fun OutlinedTextFieldValidation(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = LocalTextStyle.current,
+    label: @Composable (() -> Unit)? = null,
+    placeholder: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    error: String = "",
+    isError: Boolean = error.isNotEmpty(),
+    trailingIcon: @Composable (() -> Unit)? = {
+        if (error.isNotEmpty())
+            Icon(
+                painter = painterResource(id = TaskOasisIcons.errorCircle),
+                contentDescription = "error",
+                tint = MaterialTheme.colors.error
+            )
+    },
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    shape: Shape = MaterialTheme.shapes.small,
+    colors: TextFieldColors = TextFieldDefaults.outlinedTextFieldColors(
+        disabledTextColor = MaterialTheme.colors.disabledColor
+    )
+
+) {
+
+    Column (modifier = modifier) {
+        OutlinedTextField(
+            enabled = enabled,
+            readOnly = readOnly,
+            value = value,
+            onValueChange = onValueChange,
+            modifier = Modifier
+                .fillMaxWidth(),
+            singleLine = singleLine,
+            textStyle = textStyle,
+            label = label,
+            placeholder = placeholder,
+            leadingIcon = leadingIcon,
+            trailingIcon = trailingIcon,
+            isError = isError,
+            visualTransformation = visualTransformation,
+            keyboardOptions = keyboardOptions,
+            keyboardActions = keyboardActions,
+            maxLines = maxLines,
+            interactionSource = interactionSource,
+            shape = shape,
+            colors = colors
+        )
+        if (error.isNotEmpty()) {
+            Text(
+                text = error,
+                color = MaterialTheme.colors.error,
+                style = MaterialTheme.typography.caption,
+                modifier = Modifier.padding(start = 16.dp, top = 0.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun FilledTextFieldValidation(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = LocalTextStyle.current,
+    label: @Composable (() -> Unit)? = null,
+    placeholder: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    error: String = "",
+    isError: Boolean = error.isNotEmpty(),
+    trailingIcon: @Composable (() -> Unit)? = {
+        if (error.isNotEmpty())
+            Icon(
+                painter = painterResource(id = TaskOasisIcons.errorCircle),
+                contentDescription = "error",
+                tint = MaterialTheme.colors.error
+            )
+    },
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    shape: Shape = MaterialTheme.shapes.small,
+    colors: TextFieldColors = TextFieldDefaults.textFieldColors (
+        disabledTextColor = MaterialTheme.colors.disabledColor
+    )
+) {
+    Column (modifier = modifier) {
+        TextField(
+            enabled = enabled,
+            readOnly = readOnly,
+            value = value,
+            onValueChange = onValueChange,
+            modifier = Modifier
+                .fillMaxWidth(),
+            singleLine = singleLine,
+            textStyle = textStyle,
+            label = label,
+            placeholder = placeholder,
+            leadingIcon = leadingIcon,
+            trailingIcon = trailingIcon,
+            isError = isError,
+            visualTransformation = visualTransformation,
+            keyboardOptions = keyboardOptions,
+            keyboardActions = keyboardActions,
+            maxLines = maxLines,
+            interactionSource = interactionSource,
+            shape = shape,
+            colors = colors
+        )
+        if (error.isNotEmpty()) {
+            Text(
+                text = error,
+                color = MaterialTheme.colors.error,
+                style = MaterialTheme.typography.caption,
+                modifier = Modifier.padding(start = 16.dp, top = 0.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OutlineTextFieldValidationPreview() {
+    TaskOasisTheme() {
+        var text by remember { mutableStateOf("") }
+
+        Column(
+            Modifier
+                .background(MaterialTheme.colors.background)
+                .fillMaxSize()) {
+            OutlinedTextFieldValidation(
+                value = text,
+                onValueChange = { text = it },
+                isError = text == "error",
+                error = if (text == "error") "Error text" else "",
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    backgroundColor = MaterialTheme.colors.surface
+                )
+            )
+            Spacer(Modifier.height(20.dp))
+            FilledTextFieldValidation(
+                value = text,
+                onValueChange = { text = it },
+                isError = text == "error",
+                error = if (text == "error") "Error text" else "",
+                colors = TextFieldDefaults.textFieldColors (
+                    backgroundColor = MaterialTheme.colors.background
+                )
+            )
+        }
+    }
+}
